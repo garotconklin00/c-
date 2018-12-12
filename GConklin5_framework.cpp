7155033dec518bb89fb1e2d659e0fc4b6efa63ac
Garot Conklin <gmconklin@gmail.com>
2018-12-12 11:33:29 -0500

    Add a starting point for this.

---
+++ GConklin5_framework.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const double ECONOMY_RATE = 60.00;
+const double FULL_SIZE_RATE = 65.00;
+const double LUXURY_RATE = 125.00;
+const double FULL_TANK = 75.00;
+const double INS_COST = 8.75;
+
+// Function Prototype Declaration
+int getData(int numDays, int carClass, int gasOption, int Insurance);
+
+int main()
+{
+// function getData
+
+// function calculateCharges
+
+// function calcTotalDue
+
+// function displayBill
+		
+	return 0;
+}
+
+// function getData
+int getData(int numDays, int carClass, int gasOption, int Insurance) {
+
+}
+
+// function calculateCharges
+int calculateCharges {
+
+}
+
+// function calcTotalDue
+int calcTotalDue {
+
+}
+
+// function displayBill
+int displayBill {
+
+}
